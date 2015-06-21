e262fd09571eeb9139ded08bd83684663d069006
Gustavo Chicato <gustavo.wandeur@usp.br>
2015-06-20 23:36:20 -0300

    Finalizadas consultas

--- sql/consulta.sql
+++ sql/consulta.sql
@@ -5,9 +5,9 @@
 #--------------------------------------------------------------
 # EXEMPLO 1
 #
-# Buscar username, primeiro nome e sobrenome dos usuários
-# não suspensos que imprimiram pelo menos uma vez em
-# todas as impressoras.
+# Buscar os usernames, primeiros nomes e sobrenomes dos
+# usuários não suspensos que imprimiram pelo menos uma vez em
+# cada impressora.
 #--------------------------------------------------------------
 
 SELECT username, pNome, sNome
@@ -29,16 +29,32 @@ WHERE u.suspenso = FALSE AND NOT EXISTS (
 # destinatário sejam ambos administradores.
 #--------------------------------------------------------------
 
-
+SELECT assunto
+FROM email e
+WHERE remetente IN (
+    SELECT username
+    FROM usuario
+    WHERE ADMIN = TRUE
+) AND EXISTS (
+    SELECT *
+    FROM enviado_para, usuario u
+    WHERE 
+        destinatario = username
+        AND u.ADMIN = TRUE
+        AND idEmail = e.id
+);
 
 #--------------------------------------------------------------
 # EXEMPLO 3
 #
-# Buscar nome e versão dos pacotes que sejam simultaneamente
-# dependentes e dependência de outros pacotes.
+# Buscar nome e versão de pacotes que simultaneamente dependam
+# e sejam dependência de outros pacotes.
 #--------------------------------------------------------------
 
-SELECT nome
-FROM pacotes AS p and depende_de AS d
-WHERE p.nome = d.nome AND 
-
+SELECT nome, versao
+FROM pacote AS p, depende_de AS d
+WHERE d.nomeDep = p.nome AND EXISTS (
+    SELECT *
+    FROM depende_de AS x
+    WHERE x.nomePacote = p.nome
+);
